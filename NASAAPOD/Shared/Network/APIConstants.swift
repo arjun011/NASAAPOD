5ce4722171c9dcb98efd74be153d43a0eb6f92c9
arjun011 <arjunpatel011@hotmail.com>
2026-04-28 16:04:18 +0100

    Add API constants

---
+++ NASAAPOD/Shared/Network/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConsatnt.swift
+//  NASAAPOD
+//
+//  Created by Arjun on 28/04/26.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let APIKey = "3fi8g4ULxfqHvYNxx53PZegRmVRodePwlFSFWvM4"
+    static let baseURL = "https://api.nasa.gov/"
+}
