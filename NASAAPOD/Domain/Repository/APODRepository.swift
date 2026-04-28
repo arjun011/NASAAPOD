0e8a78a34729b948750c2ec54d7548c7b4a69ca6
arjun011 <arjunpatel011@hotmail.com>
2026-04-28 15:00:18 +0100

    Add APOD Repository protocol

---
+++ NASAAPOD/Domain/Repository/APODRepository.swift
@@ -0,0 +1,12 @@
+//
+//  APODRepository.swift
+//  NASAAPOD
+//
+//  Created by Arjun on 28/04/26.
+//
+
+import Foundation
+
+protocol APODRepository: Sendable {
+    func fetchTodayAPOD() async throws -> [APOD]
+}
