9004a2306242d88819b9f00b2caef15968d1adf2
arjun011 <arjunpatel011@hotmail.com>
2026-04-28 16:50:58 +0100

    Implement NASA Api client

--- NASAAPOD/Data/APOD/Network/NASAAPIClient.swift
+++ NASAAPOD/Data/APOD/Network/NASAAPIClient.swift
@@ -9,9 +9,20 @@ import Foundation
 
 final class NASAAPIClient: APODServiceProtocol {
     
-    func fetchTodayAPOD() async throws -> [APOD] {
-        return []
+    private var apodTodayURL: URL {
+        // Adjust path component as needed for APOD endpoint
+        URL(string: APIConstants.baseURL + "planetary/apod?api_key=\(APIConstants.APIKey)")!
     }
     
-    
+    func fetchTodayAPOD() async throws -> [APOD] {
+        let (data, response) = try await URLSession.shared.data(from: apodTodayURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              200...299 ~= httpResponse.statusCode else {
+            throw URLError(.badServerResponse)
+        }
+        
+       let apodList = try JSONDecoder().decode([APOD].self, from: data)
+        return apodList
+    }
 }
--- NASAAPOD/Shared/Network/APIConstants.swift
+++ NASAAPOD/Shared/Network/APIConstants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum APIConstants {
-    static let APIKey = "3fi8g4ULxfqHvYNxx53PZegRmVRodePwlFSFWvM4"
+    static let apiKey = "3fi8g4ULxfqHvYNxx53PZegRmVRodePwlFSFWvM4"
     static let baseURL = "https://api.nasa.gov/"
 }
